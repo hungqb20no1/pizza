357c7c24b7e7ab732f718f44da76d7c4c8182bbb
hungqb20no1 <112359357+hungqb20no1@users.noreply.github.com>
2022-10-12 23:56:42 +0700

    Add files via upload

---
+++ pizza/src/PizzaClient.java
@@ -0,0 +1,16 @@
+public class PizzaClient {
+    public void run() {
+
+        Pizza p1 = new Pizza(4.99, new Circle(2.5));
+        Pizza p2 = new Pizza(5.99, new Rectangle(6, 4));
+        PizzaDeal pd = new PizzaDeal();
+        System.out.println(p1 + " is a better deal than " + p2
+                + ": " + pd.betterDeal(p1, p2));
+
+    }
+
+    public static void main(String[] nu) {
+        new PizzaClient().run();
+    }
+
+}
